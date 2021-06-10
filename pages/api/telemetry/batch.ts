10653bae6e285cf1bb8bbc1ccbcbb7ca2c3d9f61
corporateanon <corporateanon@gmail.com>
2021-06-10 14:20:35 +0300

    allow batch loading

---
+++ pages/api/telemetry/batch.ts
@@ -0,0 +1,30 @@
+import { NextApiHandler } from 'next';
+import { Telemetry } from '../../../lib/db/models';
+
+const handler: NextApiHandler = async (req, res) => {
+    if (req.method !== 'POST') {
+        res.status(400).end();
+        return;
+    }
+
+    const telemetriesList = req.body;
+
+    const docs = [];
+    for (const telemetryItem of telemetriesList) {
+        const doc = await Telemetry.findOneAndUpdate(
+            { _id: `${telemetryItem._id}` },
+            {
+                ...telemetryItem,
+                updatedAt: Date.now(),
+            },
+            {
+                new: true,
+                upsert: true,
+            }
+        );
+        docs.push(doc);
+    }
+
+    res.status(201).json(docs);
+};
+export default handler;
